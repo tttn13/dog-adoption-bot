5207a59c73c0f7c751cea3525ee6f8e66d3f4331
Thi Nguyen <tttn.tn13@gmail.com>
2021-08-01 14:27:37 -0700

    refactor

--- bot.js
+++ bot.js
@@ -1,112 +1,117 @@
-const Twitter = require('twitter')
-require('dotenv').config({ path: './config.env' })
-const fetch = require('node-fetch')
+const Twitter = require("twitter");
+require("dotenv").config({ path: "./config.env" });
+const fetch = require("node-fetch");
 
-//Create an Instance of Twitter API and Authenticate using App Keys 
+//Create an Instance of Twitter API and Authenticate using App Keys
 const twitterClient = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_SECRET,
-})
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_SECRET,
+});
 
 const newDogsThisHour = async () => {
-    const hourAgo = new Date(new Date().getTime() - 1000 * 60 * 60).toISOString();
-    let dogsWithPhotos = []
-    try {
-        const tokenRes = await fetch('https://api.petfinder.com/v2/oauth2/token', {
-            method: 'POST',
-            body: `grant_type=client_credentials&client_id=${process.env.PF_API_KEY}&client_secret=${process.env.PF_SECRET_KEY}`,
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        })
-        const { access_token } = await tokenRes.json()
-        const dogResponse = await fetch(
-            `https://api.petfinder.com/v2/animals?type=dog&location=98121&distance=100&after=${hourAgo}`,
-            {
-                headers: {
-                    Authorization: `Bearer ${access_token}`,
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                }
-            }
-        ) 
-        const  { animals }  = await dogResponse.json()
-        if (animals.length===0) {
-            console.log("No new dogs")
-            return null
-        }
-        if (animals.length > 0){
-            console.log("Found some dogs")
-            //Filter dogs with photos
-            dogsWithPhotos = animals.filter(animal => animal.photos.length > 0)
-            return dogsWithPhotos
-        }     
-            
-    } catch (error) {
-        console.log(error)
+  const hourAgo = new Date(new Date().getTime() - 1000 * 60 * 60).toISOString();
+  let dogsWithPhotos = [];
+  try {
+    const tokenRes = await fetch("https://api.petfinder.com/v2/oauth2/token", {
+      method: "POST",
+      body: `grant_type=client_credentials&client_id=${process.env.PF_API_KEY}&client_secret=${process.env.PF_SECRET_KEY}`,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+    const { access_token } = await tokenRes.json();
+    const dogResponse = await fetch(
+      `https://api.petfinder.com/v2/animals?type=dog&location=98121&distance=100&after=${hourAgo}`,
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+    const { animals } = await dogResponse.json();
+    if (animals.length === 0) {
+      console.log("No new dogs");
+      return null;
     }
-}
+    if (animals.length > 0) {
+      console.log("Found some dogs");
+      //Filter dogs with photos
+      dogsWithPhotos = animals.filter((animal) => animal.photos.length > 0);
+      return dogsWithPhotos;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-function retweet() {
-    let params = { 
-        q: '#puppy filter:media',
-        result_type: "recent",
-        lang: "en"
-    };
-    //Send a GET Request to search/tweets inorder to get the tweets with the Query 
-    twitterClient.get("search/tweets", params, function(error, data, response) {
-            if (error) {
-                console.log("Cannot Grab Latest Tweet On Hashtag: ", params.q);
-                return; 
-            }
-            console.log("Data from API is: ", data)
-            if (data && data.statuses.length > 0) {
-                //Tweets are on an array on the searched object (data.statuses)
-                let tweet = data.statuses[0];
-                //We want to retweet only one tweet so we access the first one (0 index)
-                console.log('This is the tweet', tweet); 
-                // Retweet using the tweet's ID 
-                twitterClient.post("statuses/retweet/" + tweet.id_str, (err, res) => {
-                    if (err) {
-                        console.log("Cannot Retweet your Tweet!");
-                        return;
-                    } else {
-                        console.log("Success, Check your Account for the Retweet!");
-                    }
-                })
-            }
-     })        
-}
-
-const shareDog = async() => {
-    const newDogs = await newDogsThisHour()
-    if (newDogs) {
-        twitterClient.post(
-            'statuses/update',
-            {
-                status: `I'm looking for a home! My name is ${newDogs[0].name}. I am a ${newDogs[0].gender} ${newDogs[0].age} puppy. 
+const shareDog = async () => {
+  const newDogs = await newDogsThisHour();
+  if (newDogs) {
+    twitterClient.post(
+      "statuses/update",
+      {
+        status: `I'm looking for a home! My name is ${newDogs[0].name}. I am a ${newDogs[0].gender} ${newDogs[0].age} puppy. 
                 ${newDogs[0].url} `,
-            },
-            function(err, tweet, response) {
-                if(!err) console.log(tweet)
-                else console.log(err)
-            }
-        )
-        console.log("finish sharing")
+      },
+      function (err, tweet, response) {
+        if (!err) console.log(tweet);
+        else console.log(err);
+      }
+    );
+    console.log("finish sharing");
+  }
+};
+
+//create the Retweet function
+const retweetFn = (tweet_object) => {
+  Twitter.post(
+    "status/retweet/:id",
+    { id: tweet_object.id_str },
+    (err, data, response) => {
+      if (err) {
+        console.log("Cannot Retweet your Tweet because of", err.message);
+        return;
+      }
+      console.log("Success, Check your Account for the Retweet!");
     }
-}
+  );
+};
+
+//create a Searching function that searches tweets with the desired params
+const searchTweets = (callback) => {
+  let params = {
+    q: "#puppy filter:media",
+    count: 1,
+    result_type: "mixed",
+    lang: "en",
+  };
+
+  Twitter.get("search/tweets", params, (err, data, response) => {
+    if (data && data.statuses.length > 0) {
+      //tweets results are stored in the data.statuses array, we only want 1 tweet so we access it via index
+      let tweet = data.statuses[0];
+      console.log("This is the tweet", tweet);
+      callback(tweet); // callback function that we passed in is the retweet function => we passed in the tweet to the retweet function
+      return;
+    } else {
+      console.log("Cannot Grab Latest Tweet due to ", err.message);
+    }
+  });
+};
 
 //get data from PetFinder API and set interval for 1hr
 const adoptionDataTweets = () => {
-    shareDog()
-    setInterval(shareDog, 1000 * 60 * 60) //share every hour afterwards
-}
+  shareDog();
+  setInterval(shareDog, 1000 * 60 * 60); //share every hour afterwards
+};
+adoptionDataTweets();
+
 // #puppy retweets and set interval for 30mins
 const puppyRetweets = () => {
-    retweet()
-    setInterval(retweet, 60*30 * 1000)
-}
-
-adoptionDataTweets()
-puppyRetweets()
+  searchTweets(retweetFn);
+};
+puppyRetweets();
+setInterval(puppyRetweets, 60 * 30 * 1000);
